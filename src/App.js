cceb5a7a8aa0966eef338ab02264b9949c6a2bbc
Kevin Daniel Molina Ramirez <daniel.97.molin@gmail.com>
2021-05-18 20:26:24 -0600

    production release

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Landing from "./Components/Landing";
 import Privacy from "./Components/Privacy";
 import Header from "./Components/Header";
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+    
       <div className="App">
         <Header></Header>
-
+        
         <Route path="/home" exact component={Landing} />
         <Route path="/" exact component={Landing} />
         <Route path="/privacy" exact component={Privacy} />
+        
 
         <footer>
           <h3>503 Studios</h3>
